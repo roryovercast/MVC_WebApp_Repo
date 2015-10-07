a0163d21b7e023cc5240285f29fd025ad787beaf
Rory Overcast <rory.overcast@gmail.com>
2015-10-06 22:04:06 -0500

    added mvc

---
+++ MVC_WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_WebApp.Startup))]
+namespace MVC_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
