f0b397bdbfef1298661edfcc3b4bdb9f423dfc33
Rory Overcast <statbball@hotmail.com>
2015-10-08 12:50:19 -0500

    added scaling to chart

--- MVC_WebApp/Scripts/script.js
+++ MVC_WebApp/Scripts/script.js
@@ -1,9 +1,19 @@
-﻿var bardata = [20, 30, 105, 15];
+﻿var bardata = [40, 80, 1000, 80, 30, 70, 15, 40, 80, 40, 80, 30, 70, 15, 40, 80, 40, 80, 30, 70, 15, 40, 80];
 
 var height = 400,
     width = 600,
     barWidth = 50,
-    barOffset = 5;
+    barOffset = 5,
+    barPad = .2,
+    barOuterPad = .1;
+
+var yScale = d3.scale.linear()
+    .domain([0, d3.max(bardata)])
+    .range([0, height-10])
+
+var xScale = d3.scale.ordinal()
+    .domain(d3.range(0, bardata.length))
+    .rangeBands([0, width], barPad, barOuterPad)
 
 d3.select('#chart').append('svg')
     .attr('width', width)
@@ -12,13 +22,13 @@ d3.select('#chart').append('svg')
     .selectAll('rect').data(bardata)
     .enter().append('rect')
         .style('fill', '#C61C6F')
-        .attr('width', barWidth)
+        .attr('width', xScale.rangeBand())
         .attr('height', function (d) {
-            return d;
+            return yScale(d);
         })
         .attr('x', function(d,i) {
-            return i * (barWidth + barOffset);
+            return xScale(i);
         })
         .attr('y', function(d) {
-            return height - d;
+            return height - yScale(d);
         })
